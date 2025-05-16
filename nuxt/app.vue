554f5e381fdb7b086f5bc433800a35a3a656a1e9
LithiumValproate <minaki@matsu.rip>
2025-05-16 15:25:04 +0800

    5-16-25

---
+++ nuxt/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <div class="left"
+         style="font-size: 20px; font-weight: bold">
+      琉璃雨</div>
+    <div class="center">
+      <button>按钮</button>
+    </div>
+    <div class="right">
+      <input
+        v-model="searchQuery"
+        type="text"
+        placeholder="搜索"
+        class="search-input"
+      >
+    </div>
+  </header>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const searchQuery = ref('')
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  margin-top: 20px;
+  margin-left: 30px;
+  margin-right: 20px;
+  color: ghostwhite;
+}
+</style>
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+const title = ref('haha');
+</script>
+
+<template>
+  <div>
+    <h1 style="color: ghostwhite; background: yellow">{{ title }}</h1>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
